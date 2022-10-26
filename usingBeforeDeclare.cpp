f57d12dea7395a6b2138c2fb1f6c4d98f58d2365
bcygsws <bcygswspring168@gmail.com>
2022-10-27 05:18:24 +0800

    :memo: C++中定义变量并起名，typedef的使用

--- usingBeforeDeclare.cpp
+++ usingBeforeDeclare.cpp
@@ -11,12 +11,13 @@ using namespace std;
 //    int weight;
 //    char sex[10];
 //};
-// 2.用typeof 定义一个结构体变量并起别名
+// 2.用typedef 定义一个结构体变量并起别名
 /**
  * @ 实例
  * 参考文档：https://www.runoob.com/cplusplus/cpp-data-structures.html
- * long int *pointer37;
+ * typedef long int *pointer37;
  * pointer37 x,y,z;
+ *
  *
  */
 int max(int a, int b) {
